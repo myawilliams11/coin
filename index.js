3b919dd74fe1268b5479bf2cd6d940072bc9e7ec
Mya <35534673+myawilliams11@users.noreply.github.com>
2018-03-15 10:37:57 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-var coin = { "0", "1" 
-    state: 0, 1
-    flip: function getFlip(i= 0; i >= 1; i ++) {
-            return Math.random();
-          }
+var coin = { 
+    state: 0,
+    flip: function (){
+        Math.round(math.random()*2);
+       console.log(this.state); 
+
+//in the above function, you'll want to generate either a *rounded* 0 or a 1 *randomly*.
     },
-    toString: function() {
+    toString: function () {
         if (coin === 0){
-            document.getElementById("heads.png")
+
+            document.getElementById("heads.png").src;
         }
         else {
-            document.getElementById("tails.png")
+            document.getElementById("tails.png").src;
         }
     },
     toHTML: function() {
@@ -17,4 +20,10 @@ var coin = { "0", "1"
         // set the properties of the image element to show either heads or tails
         return img;
     }
+
+//    for (i = 0; i < 20 ; i++) {}
+//     // add for loop to flip 20x
 };
+
+for (i = 0; i < 20 ; i++) {}
+// add for loop to flip 20x
